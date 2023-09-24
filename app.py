b29635f5c0af3629d0cb70cc7902282009c3e6df
fuchs <fuchsmath@gmail.com>
2023-09-24 18:33:30 +0200

    change code such that files are not stored on server, only locally

--- app.py
+++ app.py
@@ -7,10 +7,8 @@ import os
 app = Flask(__name__)
 Bootstrap(app)
 
-UPLOAD_FOLDER = 'uploads'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 current_image_index = 0
 images = []
@@ -37,13 +35,8 @@ def uploaded_file(filename):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method == 'POST':
-        uploaded_files = request.files.getlist("file[]")
-        handle_uploaded_files(uploaded_files)
-        return redirect(url_for('index'))
 
-    return render_template('index.html', image=images[current_image_index] if images else None)
-
+    return render_template('index.html')
 
 @app.route('/previous_image', methods=['POST'])
 def previous_image():
@@ -56,8 +49,6 @@ def record_click(x, y):
     return jsonify({"x": x, "y": y})
 
 
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/log_clicks', methods=['POST'])
 def log_clicks():
@@ -66,23 +57,7 @@ def log_clicks():
         f.write(data['log'] + '\n')
     return jsonify({"status": "success"})
 
-
-def handle_uploaded_files(uploaded_files):
-    global images, current_image_index
-
-    if not os.path.exists(UPLOAD_FOLDER):
-        os.makedirs(UPLOAD_FOLDER)
-
-    filenames = []
-    for file in uploaded_files:
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            file.save(filepath)
-            filenames.append(filepath.replace("\\", "/"))
-
-    images = filenames
-    current_image_index = 0
+images_uploaded = False
 
 
 if __name__ == "__main__":
